7b63c85884b13a68770c067c8a57154fafe20abe
Satomi Richardson <strichardson@ucdavis.edu>
2023-05-30 18:13:00 -0700

    version 2 added

--- capstone/development/script.js
+++ capstone/development/script.js
@@ -11,7 +11,7 @@
         {
             for (var j=0; j<columns; j++)
             {
-                pieces+= `<div class='piece'><img src='image/puzzles/seal${place}.jpg' width=100 data-order=${order}></div>`;
+                pieces+= `<div class='piece'><img src='image/puzzles/seal${place}.jpg' width='100'></div>`;
                 place++;
                 order++;
             }
@@ -21,33 +21,34 @@
         $("#btnStart").click(function(){
             console.log("button clicked")
             var pieces = $("#puzzleContainer div");
+            var counter = 0;
             pieces.each(function(){
                 var leftPosition = Math.floor(Math.random()*290)+25 + "px";
                 var topPosition = Math.floor(Math.random()*290)+25 + "px";
+                $(this).attr('data-order', counter);
+
                 $(this).addClass("draggablePiece").css({
                     position: "absolute", 
                     left: leftPosition,
                     top: topPosition
                 })
+                counter++;
                 $("pieceContainer").append($(this));
             });
             var emptyString = "";
+            var counter = 0;
             for (var i=0; i<rows; i++) {
                 for (var j=0; j<columns; j++)
                 {
-                    emptyString+= `<div class='piece droppableSpace' data-location='${j}'></div>`;
+                    emptyString+= `<div class='piece droppableSpace' data-location='${counter}'></div>`;
+                    counter++;
                 }
                 
-                // /*match the image# and location#*/
-                // if ($j == $place) {
-                //     window.confirm('You placed in the right place')
-                // } else {
-                //     message = "Good evening";
-                //   }
             }
             $("#puzzleContainer").append(emptyString);
             $(this).hide();
             $("#btnReset").show();
+            $("#btnDone").show();
             implementLogic();
         });
         function implementLogic(){
@@ -68,17 +69,38 @@
                 drop:function(event,ui){
                     var draggableElement = ui.draggable;
                     var droppedOn = $(this);
+                    var squareNumber = $(this).attr("data-location");
+                    var pieceNumber = $(draggableElement).attr('data-order');
+                    console.log(pieceNumber);
                     droppedOn.addClass("piecePresent");
                     $(draggableElement).addClass("droppedPiece").css({
                         top:0,
                         left: 0,
                         position: "relative"
                     }).appendTo(droppedOn);
+                    if (squareNumber == pieceNumber){
+                       // alert("correct!");
+                        var popup = `#piece${pieceNumber}`;
+                        $(popup).attr('class', 'showing');
+
+                        // Add click event to close the popup
+                        $(popup).click(function(){
+                            $(this).removeClass('showing');
+                        });
+
+                    }
+                    else{alert('incorrect')};
                 }
+
             });
+
+            //close the window
+
+
             $("#btnReset").click(function(){
                 window.location.reload();
             })
+
         }
     })
 })();
